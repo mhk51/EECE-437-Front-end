898d4ecb9869064d7ebac488590ad864fe5eb39e
Charbel <cea19@mail.aub.edu>
2022-05-26 00:09:51 +0300

    Bot page

--- bot-frontend/src/Transactions.js
+++ bot-frontend/src/Transactions.js
@@ -23,6 +23,17 @@ function Transactions(){
   let [userTransactions, setUserTransactions] = useState([]);
   let [pendingTransactions, setPendingTransactions] = useState([]);
   const [selectionModel, setSelectionModel] = useState([]);
+  let [Risk, setRisk] = useState("");
+  let [buyUsdRate, setBuyUsdRate] = useState(null);
+  let [sellUsdRate, setSellUsdRate] = useState(null);
+  let [lbpCalcInp, setlbpCalcIn] = useState("");
+  let [usdCalcInp, setusdCalcInp] = useState("");
+  let [transTypeCalc, settransTypeCalc] = useState("usd-to-lbp");
+  let [usdFunds, setUsdFunds] = useState("null");
+  let [lbpFunds, setLbpFunds] = useState("null");
+  let [graph, setGraph] = useState(null);
+  let [coin_name, setCoin_name] = useState(null);
+
 
   async function addItem(transtype) {
     var type_trans_val = false;
@@ -146,6 +157,54 @@ function Transactions(){
     }
   }, [fetchPendingTransactions, userToken]);
 
+  function Start_bot(){
+    return fetch(`${SERVER_URL}/activateBot`, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": 'Bearer ' + getUserToken(),
+      }
+    })
+      .then((response) => response.json())
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+  function Stop_bot(){
+    return fetch(`${SERVER_URL}/deactivateBot`, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": 'Bearer ' + getUserToken(),
+      }
+    })
+      .then((response) => response.json())
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
+  function Change_param(){
+    const data = {
+      coin_name: coin_name,
+      risk: usdCalcInp,
+      percentage_change: lbpCalcInp
+    };
+    return fetch(`${SERVER_URL}/chnage_param`, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": 'Bearer ' + getUserToken(),
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 
   return(
         <div className='transactions'>
@@ -176,7 +235,7 @@ function Transactions(){
           </div>
           <ul className="nav navbar-nav">
             <li><NavLink activeclassname="current" to="/">Home</NavLink></li>
-            <li><NavLink activeclassname="current" to="/transactions">Transactions</NavLink></li>
+            <li><NavLink activeclassname="current" to="/transactions">Bot</NavLink></li>
             <li><NavLink activeclassname="current" to="/statistics">Statistics</NavLink></li>
           </ul>
 
@@ -194,82 +253,16 @@ function Transactions(){
       </nav>
       </div>
       <div className="row">
-            <div className="col-md-6">
-                <div className="jumbotron-at-top"><h1 className="margin">RECORD YOUR TRANSACTIONS</h1></div>
-            </div>
-            <div className="col-md-6">
-              <div className="my-container-top">
-                <div className="row">
-                  <div className="col-lg-6">
-                    <h1>LBP AMOUNT</h1>
-                  </div>
-                  <div className="col-lg-6">
-                    <h1>USD AMOUNT</h1>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-lg-6">
-                    <div className="input-group">
-                      <input
-                        className="form-control"
-                        aria-label="Amount"
-                        placeholder="LBP amount"
-                        id="amount"
-                        type="number"
-                        value={lbpInput}
-                        onChange={(e) => setLbpInput(e.target.value)}
-
-                      />
-                    </div>
-                  </div>
-                  <div className="col-lg-6">
-                    <div className="input-group">
-                      <input
-                        className="form-control"
-                        aria-label="Amount"
-                        placeholder="USD amount"
-                        id="amount"
-                        type="number"
-                        value={usdInput}
-                        onChange={(e) => setUsdInput(e.target.value)}
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div className="btn-group">
-                  <button type="button" className="btn btn-primary" onClick={() => addItem(true)}>Sell USD</button>
-                  <button type="button" className="btn btn-primary" onClick={() => addItem(false)}>Buy USD</button>
-                </div>
-              </div>
-            </div>
+      <div className="col-lg-12">
+        <div className="jumbotron bg-cover">
+          <h1>Bot</h1>
+        </div>
+      </div>
     </div>
     <div className="row available-trans">
       <div className="jumbotron available-trans">
         <h1>All available transactions</h1>
       </div>
-    </div>
-
-    <div className="table-container">
-      <h2>Pending transactions</h2>
-      <p>
-        Choose by clicking on the button in the select column the transaction
-        that you would like to complete
-      </p>
-      <DataGrid
-      id="table"
-            columns={[
-              {field: "added_date", headerName: "Date added", width: 250 },
-              {field: "lbp_amount", headerName: "LBP Amount", flex: 1 },
-              {field: "usd_amount", headerName: "USD Amount", flex: 1 },
-              {field: "usd_to_lbp", headerName: "USD to LBP", flex: 1 },
-            ]}
-            rows={pendingTransactions}
-            autoHeight
-            onSelectionModelChange={(newSelection) => {
-              setSelectionModel(newSelection);}}
-
-          />
-      <button className="submit-btn" type="button" onClick={() => completeTransaction()}>Submit</button>
     </div>
     <div className="table-container">
       <h2>User transactions</h2>
@@ -285,6 +278,107 @@ function Transactions(){
             autoHeight
 
           />
+    </div>
+    <div className="row">
+      <div className="calculator-container">
+        <h1>Options</h1>
+        <div className="row">
+          <div className="col-lg-5">
+            <h3>Risk</h3>
+          </div>
+          <div className="col-lg-2 offset-lg-6">
+            <div>
+            <i className="bi-currency-exchange" ></i>
+            </div>
+          </div>
+          <div className="col-lg-5">
+            <h3>Percentage of Use</h3>
+          </div>
+        </div>
+        <div className="row inputs">
+          <div className="col-lg-5">
+            <div className="input-group">
+              <input
+                className="form-control"
+                aria-label="Amount"
+                placeholder="Risk"
+                id="calc-lbp"
+              variant="filled"
+              type="number"
+              min="0"
+              value={lbpCalcInp}
+              onChange={(val) => {
+                  setlbpCalcIn(val.target.value);
+              }}
+              />
+            </div>
+          </div>
+          <div className="col-lg-8">
+          <div className="input-group">
+              <select val="coin_name" value={coin_name} onChange={(val)=>{setCoin_name(val.target.value)}}>
+                <option value="Bitcoin">Bitcoin</option>
+                <option value="Ethereum">Ethereum</option>
+                <option value="mercedes">Coin 3</option>
+                <option value="audi">Coin 4</option>
+              </select>
+            </div>
+            </div>
+          <div className="col-lg-5">
+            <div className="input-group">
+              <input
+                className="form-control"
+                aria-label="Amount"
+                placeholder="Percentage of Use"
+                id="calc-usd"
+              variant="filled"
+              type="number"
+              min="0"
+              value={usdCalcInp}
+              onChange={(val) => {
+                  setusdCalcInp(val.target.value);
+              }}
+              />
+            </div>
+          </div>
+        </div>
+        <br></br>
+        <div className="row">
+        <div>
+              <h3>Amount of Risk the bot would take (0 to 100)</h3>
+            </div>
+          <div className="col-lg-2 offset-lg-6">
+            <div>
+            <div class="btn-group">
+            <button type="button" className="btn btn-primary" onClick={Change_param()}>Submit</button>
+          </div>
+            </div>
+            </div>
+            <div>
+              <h3>Amount of Cash the bot would use (0 to 100)</h3>
+            </div>
+          </div>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-lg-5">
+        <div className="my-container left" >
+          <h1>Start {" "}</h1>
+          <div class="btn-group">
+            <button type="button" className="btn btn-primary" onClick={Start_bot()}>Start</button>
+            </div>
+        </div>
+      </div>
+      <div className="col-lg-2 vertical-line">
+        <div className="vl"></div>
+      </div>
+      <div className="col-lg-5">
+        <div className="my-container right">
+          <h1>Stop {" "}</h1>
+          <div class="btn-group">
+          <button type="button" className="btn btn-primary" onClick={Stop_bot()}>Stop</button>
+            </div>
+        </div>
+      </div>
     </div>
     </div>
     );
